6ba8ff8cfa14b26c4ef7cd54400cca7da97d9c2f
Syarah <syarah@users.noreply.github.com>
2020-02-27 15:48:02 +0100

    tekst aangepast read me

--- opdracht1/README.md
+++ opdracht1/README.md
@@ -1,3 +1,26 @@
+
+INSERT TEXT OF PROJECT 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Frontend voor Designers - opdracht 1: Een Micro-interactie uitwerken en testen
 
 Ontwerp een user interface voor een gegeven use case. Werk je ontwerp uit in HTML, CSS en Javascript om te kunnen testen in een browser.
--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -4,3 +4,26 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+
+
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
+
+
+
+
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    console.log(dragged)
+
+
+
+    // make it half transparent
+    event.target.style.opacity = .5;
+}, false);
